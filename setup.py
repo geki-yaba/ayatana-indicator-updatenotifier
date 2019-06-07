b0b3be04cb8c13d1fa62548aec61d780364b383c
Hanno Meyer-Thurow <h.mth@web.de>
2019-06-07 21:51:01 +0200

    setup.py: version 0.0.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ayatana-indicator-updatenotifier",
-    version="0.0.3",
+    version="0.0.4",
     author="Hanno Meyer-Thurow",
     author_email="none",
     description="A simple system packages update checker",
